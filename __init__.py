159e7ae20ef7c5866b8b2791cde5fd1db3b52166
Charge <expidite@riseup.net>
2023-06-27 23:28:15 -0500

    Connection list commit

---
+++ __init__.py
@@ -0,0 +1,26 @@
+import subprocess
+
+"""
+Python/subprocesses based Mullvad VPN CLI Package
+Used to rotate IP connections via python
+"""
+
+__version__ = 1.0
+
+
+def refresh_connections_list():
+    """ Get the list of connections and parse """
+    try:
+        output = subprocess.check_output("mullvad relay list", shell=True)
+    except subprocess.CalledProcessError:
+        exit("Mullvad Client isn't installed on your device!")
+    connection_lines: list = [line.strip("\t\t") for line in output.decode().split("\n") if line.strip("\t\t").startswith("us")]
+    us_connection_relays: list = [ ]
+    for connection_line in connection_lines:
+        if connection_line.startswith("us"):
+            us_connection_relays.append(connection_line.split(" ")[0])
+    return us_connection_relays
+
+
+avalible_connections: list = refresh_connections_list()
+
